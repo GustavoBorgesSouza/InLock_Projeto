08d32956db87ceb8967be56a93bdd4e925a0f6e3
GustavoBorges <sgustavo.borges10@gmail.com>
2021-09-09 15:28:17 -0300

    DRUD jogo funcionando

---
+++ Backend/senai_inlock_webAPI/senai_inlock_webAPI/senai_inlock_webAPI/Properties/Controllers/JogosController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai_inlock_webAPI.Properties.Domains;
+using senai_inlock_webAPI.Properties.Interfaces;
+using senai_inlock_webAPI.Properties.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_inlock_webAPI.Properties.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        private IJogoRepository _JogoRepository { get; set;  }
+
+        public JogosController()
+        {
+            _JogoRepository = new JogoRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<JogoDomain> ListaJogos = _JogoRepository.ListarTodos();
+
+                return Ok(ListaJogos);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                JogoDomain jogobuscado = _JogoRepository.BuscarPorId(id);
+
+                if (jogobuscado != null)
+                {
+                    return Ok(jogobuscado);
+                }
+                return NotFound("Jogo não encontrado");
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(JogoDomain novoJogo)
+        {
+            try
+            {
+                _JogoRepository.Cadastrar(novoJogo);
+
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _JogoRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put(JogoDomain JogoAtualizar)
+        {
+            JogoDomain JogoBuscado = _JogoRepository.BuscarPorId(JogoAtualizar.idJogo);
+
+            if (JogoBuscado != null)
+            {
+                try
+                {
+                    _JogoRepository.Atualizar(JogoAtualizar);
+                    return NoContent();
+                }
+                catch (Exception erro)
+                {
+                    return BadRequest(erro);
+                }
+            }
+
+            return NotFound(new
+            {
+                mensagem = "Jogo não encontrado",
+                Coderro = true
+            });
+        }
+    }
+}
---
+++ Backend/senai_inlock_webAPI/senai_inlock_webAPI/senai_inlock_webAPI/Properties/Repositories/JogoRepository.cs
@@ -0,0 +1,163 @@
+﻿using senai_inlock_webAPI.Properties.Domains;
+using senai_inlock_webAPI.Properties.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_inlock_webAPI.Properties.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        //Conecta pelo windows de casa do borges
+        /// <summary>
+        /// String de conexão que recebe os parâmetros
+        /// Data Source = Nome do Servidor
+        /// initial  catalog = Nome do banco de dados
+        /// user id= sa; pwd= Senai@132 = Faz a autenticação com o SQL SERVER, passando login e senha
+        /// integrated security = Faz a autenticação com o usuário do sistema(windows)
+        /// </summary>
+        private string stringConexao = "Data Source=localhost\\SQLEXPRESS01; initial catalog=inlock_games_manha; integrated security=true";
+
+        public void Atualizar(JogoDomain JogoAtualizado)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryUpdate = "UPDATE Jogo Set idEstudio = @novoEstudio, nomeJogo = @novoNome, descricao = @novaDescricao, dataLancamento = @novaDataL, valor = @novoValor WHERE idJogo = @ID;";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryUpdate,con))
+                {
+                    cmd.Parameters.AddWithValue("@novoEstudio", JogoAtualizado.idEstudio);
+                    cmd.Parameters.AddWithValue("@novoNome", JogoAtualizado.nomeJogo);
+                    cmd.Parameters.AddWithValue("@novaDescricao", JogoAtualizado.descricao);
+                    cmd.Parameters.AddWithValue("@novaDataL", JogoAtualizado.dataLancamento);
+                    cmd.Parameters.AddWithValue("@novoValor", JogoAtualizado.valor);
+
+                    cmd.Parameters.AddWithValue("@ID", JogoAtualizado.idJogo);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public JogoDomain BuscarPorId(int idProcurar)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelectbyId = "SELECT idJogo, nomeEstudio, nomeJogo, descricao, dataLancamento, valor FROM Jogo LEFT JOIN Estudio ON Jogo.idEstudio = Estudio.idEstudio WHERE idJogo = @ID;";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectbyId, con))
+                {
+                    cmd.Parameters.AddWithValue("@ID", idProcurar);
+
+                    rdr = cmd.ExecuteReader();
+
+                    if (rdr.Read())
+                    {
+                        JogoDomain jogoBuscado = new JogoDomain()
+                        {
+                            idJogo = Convert.ToInt32(rdr[0]),
+                            estudio = new EstudioDomain()
+                            {
+                                nomeEstudio = rdr[1].ToString()
+                            },
+                            nomeJogo = rdr[2].ToString(),
+                            descricao = rdr[3].ToString(),
+                            dataLancamento = Convert.ToDateTime(rdr[4]),
+                            valor = Convert.ToInt32(rdr[5])
+                        };
+
+                        return jogoBuscado;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void Cadastrar(JogoDomain novoJogo)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryInsert = @"INSERT INTO Jogo(idEstudio,nomeJogo,descricao,dataLancamento,valor)
+                VALUES(@novoEstudio, @novoNome, @novaDescricao, @novaDataL, @novoValor)";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                {
+                    cmd.Parameters.AddWithValue("@novoEstudio", novoJogo.idEstudio);
+                    cmd.Parameters.AddWithValue("@novoNome", novoJogo.nomeJogo);
+                    cmd.Parameters.AddWithValue("@novaDescricao", novoJogo.descricao);
+                    cmd.Parameters.AddWithValue("@novaDataL", novoJogo.dataLancamento);
+                    cmd.Parameters.AddWithValue("@novoValor", novoJogo.valor);
+
+                    cmd.ExecuteNonQuery();
+                };
+            }
+        }
+
+        public void Deletar(int idDeletar)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryDelete = "DELETE FROM Jogo WHERE idJogo = @idJogo;";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryDelete, con))
+                {
+                    cmd.Parameters.AddWithValue("@idJogo", idDeletar);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<JogoDomain> ListarTodos()
+        {
+            List<JogoDomain> ListaJogos = new List<JogoDomain>();
+
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelectAll = "SELECT idJogo, nomeEstudio, nomeJogo, descricao, dataLancamento, valor FROM Jogo LEFT JOIN Estudio ON Jogo.idEstudio = Estudio.idEstudio;";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        JogoDomain jogo = new JogoDomain()
+                        {
+                            idJogo = Convert.ToInt32(rdr[0]),
+                            estudio = new EstudioDomain()
+                            {
+                                nomeEstudio = rdr[1].ToString()
+                            },
+                            nomeJogo = rdr[2].ToString(),
+                            descricao = rdr[3].ToString(),
+                            dataLancamento = Convert.ToDateTime(rdr[4]),
+                            valor = Convert.ToInt32(rdr[5])
+                        };
+
+                        ListaJogos.Add(jogo);
+                    }
+                }
+
+                return ListaJogos;
+            }
+        }
+    }
+}
